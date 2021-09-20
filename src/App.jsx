a1a43cf3ed63d03830e4b8883ae9df0e6e376d3d
Aquista <kaquista@fi.uba.ar>
2021-09-20 15:10:50 -0300

    Agrego botones de Editar y Eliminar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-import React, {useState} from "react";
+import React, {useImperativeHandle, useState} from "react";
 import User from "./components/User.jsx";
 
-export function App() {
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            users: [
+                {id: 1, firstName: "Pedro", lastName: "Lopez", email: "pedro.cabj@test.com", role: "User"},
+                {id: 2, firstName: "Victoria", lastName: "Gonzalez", email: "vic2000@test.com", role: "User"},
+                {id: 3, firstName: "Juan", lastName: "Gutierrez", email: "juan@test.com", role: "Admin"},
+                {id: 4, firstName: "Lucas", lastName: "Garcia", email: "lucasgarcia@test.com", role: "User"},
+                {id: 5, firstName: "Sofia", lastName: "Fernandez", email: "sofer@test.com", role: "Admin"},
+            ]
+        };
+    }
 
-    const [users, setUsers] = useState([
-        {id: 1, firstName: "Pedro", lastName: "Lopez", email: "pedro.cabj@test.com", role: "User"},
-        {id: 2, firstName: "Victoria", lastName: "Gonzalez", email: "vic2000@test.com", role: "User"},
-        {id: 3, firstName: "Juan", lastName: "Gutierrez", email: "juan@test.com", role: "Admin"},
-        {id: 4, firstName: "Lucas", lastName: "Garcia", email: "lucasgarcia@test.com", role: "User"},
-        {id: 5, firstName: "Sofia", lastName: "Fernandez", email: "sofer@test.com", role: "Admin"},
-    ]);
 
-    return (
-        <div className = "container">
-            <h3 className = "p-3 text-center"> Lista de usuarios </h3>
-            <table className="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Email</th>
-                        <th>Rol</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => {
-                        return <User userData={user}/>
-                    })}
-                </tbody>
-            </table>
-        </div>
+    deleteUser = function(userId) {
+        let users = [...this.state.users];
+        users = users.filter((user) => {
+            return user.id !== userId
+        });
+        this.setState({users : users});
+        
+    }
 
-    );
+    render() {
+        return (
+            <div className = "container">
+                <h3 className = "p-3 text-center"> Lista de usuarios </h3>
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>Rol</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.users.map((user) => {
+                            return <User userData={user} onClick={this.deleteUser.bind(this)}/>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
+
+export default App;
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 class User extends React.Component {
     render(){
         return (
@@ -7,6 +6,12 @@ class User extends React.Component {
                 <td>{this.props.userData.firstName} {this.props.userData.lastName}</td>
                 <td>{this.props.userData.email}</td>
                 <td>{this.props.userData.role}</td>
+                <td>
+                    <div class="btn-group" role="group">
+                        <button className="btn btn-warning">Editar</button>
+                        <button className="btn btn-danger" onClick={() => this.props.onClick(this.props.userData.id)}>Eliminar</button>
+                    </div>
+                </td>
             </tr>
         );
     }
