f7bea5be990a7b62cb288ff4d49e4892db063770
azulzaietz <azulzaietz@gmail.com>
2021-10-09 16:21:43 -0300

    UBADEMY 133 - modelado ui de data table users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,47 +1,62 @@
 import React from 'react';
-import User from "../components/users/User.jsx";
+
+import DataTableAdmin from '../components/DataTableAdmin.jsx';
+
+const USERS_ENDPOINT_URL = ""; //TODO CONECTAR API 
+
 class Users extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
-            users: [
-                {id: 1, firstName: "Pedro", lastName: "Lopez", email: "pedro.cabj@test.com", role: "User"},
-                {id: 2, firstName: "Victoria", lastName: "Gonzalez", email: "vic2000@test.com", role: "User"},
-                {id: 3, firstName: "Juan", lastName: "Gutierrez", email: "juan@test.com", role: "Admin"},
-                {id: 4, firstName: "Lucas", lastName: "Garcia", email: "lucasgarcia@test.com", role: "User"},
-                {id: 5, firstName: "Sofia", lastName: "Fernandez", email: "sofer@test.com", role: "Admin"},
-            ]
-        };
     }
 
-    deleteUser = function(userId) {
-        let users = [...this.state.users];
-        users = users.filter((user) => {
-            return user.id !== userId
-        });
-        this.setState({users : users});
-        
+    fetchUsers = async page => {
+        /*
+		setLoading(true);
+
+		const response = await axios.get(`https://reqres.in/api/users?page=${page}&per_page=${perPage}&delay=1`);
+
+		setData(response.data.data);
+		setTotalRows(response.data.total);
+		setLoading(false);
+        */
+	};
+
+    //ADD AND SEND TO DATA TABLE ADMIN BY PROPS
+    /*
+    const handlePageChange = page => {
+		fetchUsers(page);
+	};
+
+	const handlePerRowsChange = async (newPerPage, page) => {
+		setLoading(true);
+
+		const response = await axios.get(`https://reqres.in/api/users?page=${page}&per_page=${newPerPage}&delay=1`);
+
+		setData(response.data.data);
+		setPerPage(newPerPage);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		fetchUsers(1); // fetch page 1 of users
+		
+	}, []);
+     */
+
+
+    setColumns = () => {
+        //return columns;
     }
 
     render() { 
         return (
         <div className='Users'>
-            <h3 className = "p-3 text-center"> User list </h3>
-            <table className="UserTable table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Options</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.users.map((user) => {
-                        return <User userData={user} onClick={this.deleteUser.bind(this)}/>
-                    })}
-                </tbody>
-            </table>
+            <DataTableAdmin 
+                className="data-table-users"
+                title="Users"
+                data={this.fetchUsers}
+                columns={this.setColumns}
+            ></DataTableAdmin>
         </div>
     )}
 }
