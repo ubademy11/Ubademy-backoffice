84a80e54efeed084749a1ea5d77eba2b31351028
manulon <mlongoe@fi.uba.ar>
2021-10-15 18:55:11 -0300

    agrego course

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import "./css/app/Navbar.css";
 import "./css/app/SignUp.css";
 import "./css/app/SignIn.css";
 import "./css/courses/Courses.css";
+import "./css/courses/Course.css";
 import "./css/users/Users.css";
 import Sidebar from "./components/Sidebar.jsx";
 import Navbar from "./components/Navbar.jsx";
@@ -14,6 +15,7 @@ import Users from "./pages/Users.jsx";
 import Courses from "./pages/Courses.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import SignIn from "./pages/SignIn.jsx";
+import Course from "./pages/Course.jsx";
 
 class App extends React.Component{
     constructor(props){
@@ -26,6 +28,7 @@ class App extends React.Component{
                 <Switch>
                     <Route path="/" exact component={SignIn} />
                     <Route path="/signup" exact component={SignUp} />
+                    <Route path="/dummyCourse" exact component={Course} />
                     <div className = "App">
                         <Navbar/>
                             <div className="MainContainer">
---
+++ src/pages/Course.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import axios from 'axios';
+const COURSES_ENDPOINT_URL = "http://localhost:3002/course/byParams?title=curso%de%excel%1";
+
+class Course extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            course_info: []   
+        };
+    }
+    
+    goToSignIn = event => {
+        event.preventDefault();
+        window.location.href = "./dummyCourse";
+    }
+
+    fetchCourses = async () => {
+		try{
+            const response = await axios.get(COURSES_ENDPOINT_URL);
+            this.setState({course_info: response.data.courses});
+            console.log("en el setState");
+            console.log(this.course_info);
+        }catch(error) {
+            console.log("Error al buscar los cursos");
+            console.log(error);
+        }
+	};
+
+    componentDidMount () {
+        this.fetchCourses();
+        console.log("en el didMount");
+        console.log(this.course_info);
+    }
+
+    render () {
+        return(
+            <div className="coursePage">
+                <header>
+                    <href className="header-logo-info">
+                        <img className="header-logo" src={"https://i.ibb.co/GChKvms/Whats-App-Image-2021-10-01-at-16-32-08.jpg"}></img>
+                    </href>
+                </header>          
+
+                <div className="courseTopic">hooo</div>
+                <div className="courseInfo">
+                    <div><img className="courseLogo" src={"https://i.ibb.co/tmGCPSy/python-para-principiantes.jpg"}></img></div>
+                    <div className="courseTitle">haha</div>
+                    <div className="courseTitleSecondary">En este curso aprenderás desde las bases hasta POO en Python.<br/> 
+                    Empieza a Aprender ya Mismo de forma Gratuita.</div>
+                    {/*<div><img className="courseLogo" src={"https://i.ibb.co/mvvm1Ry/logo-television.png"}></img></div>*/}
+                    <div className="courseDetails">5h 20min de video bajo demanda.</div><br/>
+                    <div className="courseDetails">Creado por Alvaro Chirou • 380.000+ Students Worldwide, Academia AC, Universidad Hacking, Walter Coto.</div><br/>
+                    <div className="courseDetails">Español.</div>                
+                </div>
+
+                <div className="adminInfo">
+                    <div className="adminTitle">Instructor</div>
+                    <div><img className="adminPhoto" src={"https://i.ibb.co/Pjjh5TY/admin-dummy100100.png"}></img></div>
+                    <div className="adminName">Alvaro Chirou • 380.000+ Students Worldwide</div>
+                    <div className="adminGrades">Digital Technology Teacher | Seguridad informática y Forense</div>
+                    <div className="adminDetails">Calificacion del instructor: 4,5</div><br/>
+                    <div className="adminDetails">51 cursos</div>
+                </div>
+
+                {/*<Link to="/Users">Login</Link>*/}
+            </div>
+        );
+    }
+}
+
+export default Course;
