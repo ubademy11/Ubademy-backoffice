3c9de79a6169bb5c8a07ca72e34b86dcb879af31
azulzaietz <azulzaietz@gmail.com>
2021-10-18 17:50:25 -0300

    circular progress para cargar datos api

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import axios from 'axios';
+import { CircularProgress } from '@material-ui/core';
 
 class Course extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            courseInfo: []
+            courseInfo: [],
+            loading: true
         };
         this.endpoint = "https://ubademy-course-service.herokuapp.com/course/byParams" + window.location.search;
     }
@@ -18,6 +20,8 @@ class Course extends React.Component {
             console.log("Error al buscar el curso solicitado");
             console.log(error);
         }
+
+        this.setState({ loading: false });
 	};
 
     componentDidMount () {
@@ -26,26 +30,28 @@ class Course extends React.Component {
 
     render () {
         console.log('th', this.state);
-        console.log(window.location.search);
-        return(
-            <div className="coursePage">    
-                <div className="courseTopic">{this.state.courseInfo.title}</div>
-                
-                <div><img className="courseLogo" src={"https://i.ibb.co/tmGCPSy/python-para-principiantes.jpg"}></img></div>
-                <div className="courseTitle">{this.state.courseInfo.title}</div>
-                <div className="courseTitleSecondary">{this.state.courseInfo.subtitle}</div>
-                <div className="courseDescription">Descripción: {this.state.courseInfo.description}</div>
-                <div className="courseID">ID creador: {this.state.courseInfo.creatorId}</div>
-                <div className="courseLanguage"> Lenguaje: {this.state.courseInfo.language}</div>                
-                
-                <div className="adminTitle">Instructor</div>
-                <div><img className="adminPhoto" src={"https://i.ibb.co/Pjjh5TY/admin-dummy100100.png"}></img></div>
-                <div className="adminName">Alvaro Chirou • 380.000+ Students Worldwide</div>
-                <div className="adminGrades">Digital Technology Teacher | Seguridad informática y Forense</div>
-                <div className="adminRate">Calificacion del instructor: 4,5</div>
-                <div className="adminCourses">51 cursos</div>
-            </div>
-        );
+        let curso;
+        if(this.state.loading)
+            curso = <CircularProgress />; //TODO: cambiar ubicacion
+        else 
+            curso = <div className="coursePage">    
+                        <div className="courseTopic">{this.state.courseInfo.title}</div>
+                        
+                        <div><img className="courseLogo" src={"https://i.ibb.co/tmGCPSy/python-para-principiantes.jpg"}></img></div>
+                        <div className="courseTitle">{this.state.courseInfo.title}</div>
+                        <div className="courseTitleSecondary">{this.state.courseInfo.subtitle}</div>
+                        <div className="courseDescription">Descripción: {this.state.courseInfo.description}</div>
+                        <div className="courseID">ID creador: {this.state.courseInfo.creatorId}</div>
+                        <div className="courseLanguage"> Lenguaje: {this.state.courseInfo.language}</div>                
+                        
+                        <div className="adminTitle">Instructor</div>
+                        <div><img className="adminPhoto" src={"https://i.ibb.co/Pjjh5TY/admin-dummy100100.png"}></img></div>
+                        <div className="adminName">Alvaro Chirou • 380.000+ Students Worldwide</div>
+                        <div className="adminGrades">Digital Technology Teacher | Seguridad informática y Forense</div>
+                        <div className="adminRate">Calificacion del instructor: 4,5</div>
+                        <div className="adminCourses">51 cursos</div>
+                    </div>;
+        return ( <div>{curso}</div>);
     }
 }
 
