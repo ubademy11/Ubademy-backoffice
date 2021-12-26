e1172a6dd21bdcfa5a89d350c2c37b248965808a
Aquista <kaquista@fi.uba.ar>
2021-12-26 16:38:08 -0300

    Cambio refresh metricas

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 
-//export const USER_SERVICE_BASE_URL = "http://localhost:3002";
+export const USER_SERVICE_BASE_URL = "http://localhost:3002";
 //export const COURSE_SERVICE_BASE_URL = "http://localhost:3001";
 
-export const USER_SERVICE_BASE_URL = "https://user-service-distance71.cloud.okteto.net";
+//export const USER_SERVICE_BASE_URL = "https://user-service-distance71.cloud.okteto.net";
 export const COURSE_SERVICE_BASE_URL = "https://course-service-distance71.cloud.okteto.net";
 
 // USER SERVICE
@@ -12,6 +12,7 @@ export const USER_INFO_URL = USER_SERVICE_BASE_URL + "/user/me";
 export const SEARCH_USER_BY_PARAMS_URL = USER_SERVICE_BASE_URL + "/user/profile";
 export const METRICS_URL = USER_SERVICE_BASE_URL + "/metrics";
 export const METRICS_INTERVAL_URL = USER_SERVICE_BASE_URL + "/metric/latest?interval=";
+export const METRICS_REFRESH_URL = USER_SERVICE_BASE_URL + "/metric/refresh";
 
 // COURSE SERVICE
 export const COURSE_LIST_URL = COURSE_SERVICE_BASE_URL + "/course";
